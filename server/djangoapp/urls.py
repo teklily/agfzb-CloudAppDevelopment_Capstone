cedfd9213919a35196946dead87808b567bef9fe
teklily <lilianaweb@teklily.com>
2022-11-02 22:13:25 -0700

    Save 1
    First save point

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-
+    path(route='about', view=views.about, name='about'),
     # path for contact us view
-
+    path(route='contact', view=views.contact, name='contact'),
     # path for registration
 
     # path for login
@@ -25,4 +25,5 @@ urlpatterns = [
 
     # path for add a review view
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
+ + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
